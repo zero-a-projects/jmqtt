c1d87ba5677b4fd1f5b12aeab0d81a715f73e23b
杨先生其实很无奈 <2444864349@qq.com>
2020-10-17 00:20:48 +0800

    新增客户端获取接口

--- jmqtt-remoting/src/main/java/org/jmqtt/remoting/netty/RequestProcessor.java
+++ jmqtt-remoting/src/main/java/org/jmqtt/remoting/netty/RequestProcessor.java
@@ -2,7 +2,6 @@ package org.jmqtt.remoting.netty;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.mqtt.MqttMessage;
-import org.jmqtt.common.model.Message;
 
 public interface RequestProcessor {
 
