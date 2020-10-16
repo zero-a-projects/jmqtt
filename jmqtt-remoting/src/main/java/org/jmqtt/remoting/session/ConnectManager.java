f9ea4c3970fc9e3fb99283970c5ef679b6f8e317
杨先生其实很无奈 <2444864349@qq.com>
2020-10-17 00:23:06 +0800

    修改分支名称

--- jmqtt-remoting/src/main/java/org/jmqtt/remoting/session/ConnectManager.java
+++ jmqtt-remoting/src/main/java/org/jmqtt/remoting/session/ConnectManager.java
@@ -3,7 +3,6 @@ package org.jmqtt.remoting.session;
 import org.jmqtt.common.log.LoggerName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
